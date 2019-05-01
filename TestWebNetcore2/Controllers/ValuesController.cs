13b481d609961164acf207400138b0d66de29d13
Tony Alcast <tony.alcast@gmail.com>
2019-05-01 18:17:59 -0500

    Agregar archivos de proyecto.

---
+++ TestWebNetcore2/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebNetcore2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("values")]
+        public List<String> Get()
+        {
+            List<String> resultado = new List<String>
+            {
+                "API en .Net Core 2.1.",
+                "Implementada por TonyAlcast."
+            };
+            return resultado;
+        }
+    }
+}
